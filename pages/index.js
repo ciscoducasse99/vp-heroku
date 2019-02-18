943b29f1e5b0c312c52e442578f59732c10eb95f
Francisco Ducasse <ciscoducasse99@hotmail.com>
2019-02-18 15:35:20 -0500

    fixing index pages again

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-//Use 'npm run dev' to run app
-
 import Layout from '../components/Layout/Layout';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -19,9 +17,9 @@ const INDEX = () => (
             <div id="home-page-header" className="pb-sm-0">
                 <div className="text-center">
                     <h1 className="display-4 lead"><strong>Meet Via.</strong></h1>
-                    <p className="col-10 col-xl-5 mb-5 mx-auto" id="p-desc">Express yourself with creative camera tools and effects and easily share your photos and videos with friends.</p>
+                    <p className="col-10 col-xl-5 mx-auto pb-5" id="p-header">Express yourself with creative camera tools and effects and easily share your photos and videos with friends.</p>
                     <Sphere />
-                    <p className="text-white">Click above to get started or <Link href="/websites"><a style={{color:'#0076ff'}}>Learn more.</a></Link></p>
+                    <p id="p-desc" className="text-white pt-3">Click above to get started or <Link href="/websites"><a style={{color:'#0076ff'}}>Learn more.</a></Link></p>
                 </div>
             </div>
         </div>
