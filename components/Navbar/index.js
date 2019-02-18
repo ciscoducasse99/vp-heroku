558ce44eb26aee44fdaf5ba21d3f96548d45a4b2
Francisco Ducasse <ciscoducasse99@hotmail.com>
2019-02-18 15:40:49 -0500

    fixing / pages again

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -7,19 +7,9 @@ const NAVBAR = () => (
     <Link href="/"><a className="navbar-brand lead mr-0">The Vision Project</a></Link>
     <div className="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
       <ul className="navbar-nav mx-auto text-center">
-        <li className="nav-item active">
-          <a className="nav-link" href="#"></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#"></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#"></a>
-        </li>
+       
       </ul>
       <ul className="nav navbar-nav flex-row justify-content-center flex-nowrap">
-        <li className="nav-item"><a className="nav-link" href=""></a> </li>
-        <li className="nav-item"><a className="nav-link" href=""></a> </li>
       </ul>
     </div>
   </nav>
