4f7a5fa050d24a223afef38567202081112f0d82
Francisco Ducasse <ciscoducasse99@hotmail.com>
2019-02-21 21:11:02 -0500

    getting the status bar to work

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const INDEX = () => (
         <div id="home-main-section" className="m-0 p-0">
             <div id="home-page-header" className="text-center">
                 <h1 className="display-4 lead"><strong>Meet Via.</strong></h1>
-                <p className="col-10 col-xl-5 mx-auto pb-5" id="p-header">Express yourself with a modern and innovative website to provide a platform for your new clients and your community, for free.</p>
+                <p className="col-11 col-xl-5 mx-auto pb-5" id="p-header">Express yourself with a modern and innovative website to provide a platform for your new clients and your community, <a className="text-white">for free. </a></p>
                 <Sphere />
                 <p id="p-desc" className="text-white pt-3">Click above to get started or <Link href="/websites"><a style={{ color: '#0076ff' }}>learn more.</a></Link> </p>
             </div>
