41326ae6289a9ed84cddb066dd7f16369f9d46ad
Francisco Ducasse <ciscoducasse99@hotmail.com>
2019-02-23 15:01:33 -0500

    getting the status bar to work

--- pages/via-beta.js
+++ pages/via-beta.js
@@ -1,8 +1,9 @@
 import Layout from '../components/Layout/Layout';
 import ViaBeta from '../components/Chatbot-Beta/Chatbot';
-
+import Sphere from '../components/Sphere/index';
 const ViaBetaExport = () =>(
     <Layout>
+        <Sphere/>
         <ViaBeta/>
     </Layout>
 );
