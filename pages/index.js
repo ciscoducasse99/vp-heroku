5ebd45e54a2ad57fc645c057b8a5d4b914d351fa
Francisco Ducasse <ciscoducasse99@hotmail.com>
2019-03-17 23:45:20 -0400

    index-airbnb

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,12 @@ const INDEX = () => (
                 <Sphere />
                 <p id="p-desc" className="pt-3">Click above to get started or <Link href="/websites"><a style={{ color: 'white', textDecoration: 'underline' }}>learn more.</a></Link> </p>
             </div>
-            <div class="airbnb-embed-frame" data-id="33018372" data-view="home" style="width:450px;height:300px;margin:auto"><a href="https://www.airbnb.com/rooms/33018372?s=51">View On Airbnb</a><a href="https://www.airbnb.com/rooms/33018372?s=51" rel="nofollow">Waffle Haus *Newly Remodeled* Close to Downtown</a><script async="" src="https://www.airbnb.com/embeddable/airbnb_jssdk"></script></div>
-            <div class="airbnb-embed-frame" data-id="32994769" data-view="home" style="width:450px;height:300px;margin:auto"><a href="https://www.airbnb.com/rooms/32994769?s=51">View On Airbnb</a><a href="https://www.airbnb.com/rooms/32994769?s=51" rel="nofollow">New Luxurious Townhome with Beautiful Valley Views</a><script async="" src="https://www.airbnb.com/embeddable/airbnb_jssdk"></script></div>
+            <div ><a href="https://www.airbnb.com/rooms/33018372?s=51">View On Airbnb</a><a href="https://www.airbnb.com/rooms/33018372?s=51" rel="nofollow">Waffle Haus *Newly Remodeled* Close to Downtown</a>
+            </div>
+            <div >
+            <a href="https://www.airbnb.com/rooms/32994769?s=51">View On Airbnb</a>
+            <a href="https://www.airbnb.com/rooms/32994769?s=51" rel="nofollow">New Luxurious Townhome with Beautiful Valley Views</a>
+            </div>
         </div>
 
     </Layout>
