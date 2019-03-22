2c0f2489abbf3455f80ec10197a883e8ba9d0e37
Francisco Ducasse <ciscoducasse99@hotmail.com>
2019-03-21 21:36:32 -0400

    new index

--- pages/websites.js
+++ pages/websites.js
@@ -11,26 +11,24 @@ const WebDev = () => (
 
         <div id="webdev-page">
             <div id="vp-welcoming-section" className="">
-                <div className="" id="vp-masthead-content">
-                    <div className="text-center">
-                        <div className="col-lg-10 mx-auto">
-                            <p className="text-dark text-uppercase font-weight-bold font-header">Welcome to The Vision Project</p>
-                            <h1 className=""><strong>What would you do if you can do anything you want?</strong></h1>
-                        </div>
-                        <div className="col-lg-8 mx-auto">
-                            <p className="lead">My best way to help is to develop FREE websites for those looking to go into a new profession...with no strings attached.</p>
-                        </div>
-                        <a className="btn vp-button">Connect with Cisco</a>
+                <div id="vp-welcoming-content" className="text-center pt-5">
+                    <div className="col-lg-10 mx-auto mt-1 mt-sm-1">
+                        <p className="text-dark text-uppercase font-weight-bold font-header">Welcome to The Vision Project</p>
+                        <h1 className="w-sm-75"><strong>What would you do if you can do anything you want?</strong></h1>
                     </div>
+                    <div className="col-lg-8 mx-auto">
+                        <p className="lead w-75 mx-auto">Our best way to help is to develop FREE websites for those looking to go into a new profession...with no strings attached.</p>
+                    </div>
+                    <a className="btn vp-button">Connect with Cisco</a>
                 </div>
             </div>
 
             <section id="vp-services-section" className="">
-                <div className="container">
+                {/* <div className="container">
                     <div className="row">
                         <div className="text-center">
-                            <h3 className="subhead">What We Do</h3>
-                            <h1 className="heading text-center">We’ve got everything you need to launch and expand your business online</h1>
+                            <h6 className="mx-auto">What We Do</h6>
+                            <h2 className="heading mx-auto w-sm-75 ">Everything you need to expand yourself online</h2>
                         </div>
                     </div>
                     <div className="row py-4">
@@ -80,7 +78,61 @@ const WebDev = () => (
                             </div>
                         </div>
                     </div>
+                </div> */}
+
+                <div className="container py-5">
+                    <div className="row py-3">
+                        <div className="col-md-6">
+                            <div className="">
+                                <h1>How we get things done</h1>
+                                <p className="w-75">We're a small team, but this is how we try to be as efficent as possible.</p>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="d-flex d-row">
+                                <div className="p-3 align-self-start">
+                                    <i className="fa fa-diamond fa-3x"></i>
+                                </div>
+                                <div className="p-3 align-self-end">
+                                    <div className="text-center">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores nisi nihil.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="d-flex d-row">
+                                <div className="p-3 align-self-start">
+                                    <i className="fa fa-diamond fa-3x"></i>
+                                </div>
+                                <div className="p-3 align-self-end">
+                                    <div className="text-center">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores nisi nihil.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="d-flex d-row">
+                                <div className="p-3 align-self-start">
+                                    <i className="fa fa-diamond fa-3x"></i>
+                                </div>
+                                <div className="p-3 align-self-end">
+                                    <div className="text-center">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores nisi nihil.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="d-flex d-row">
+                                <div className="p-3 align-self-start">
+                                    <i className="fa fa-diamond fa-3x"></i>
+                                </div>
+                                <div className="p-3 align-self-end">
+                                    <div className="text-center">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores nisi nihil.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </section>
 
             <div id="vp-vision-section">
